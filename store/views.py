ef1b87cd50fe36d3f911107e6045ed45678ef378
Shakib Hossain <shakib.hossain@gogames.co>
2019-12-30 03:00:33 +0600

    Refactor store/views.py

--- store/views.py
+++ store/views.py
@@ -12,25 +12,21 @@ class ValuesAPIView(views.APIView):
     def get(self, request):
         get_params = request.GET.get('keys')
         if get_params is None:
+            # Get all available keys
             keys = cache.keys('*')
         else:
             keys = get_params.split(',')
-        # Reset TTL on every GET request
-        for key in keys:
-            cache.touch(key, DEFAULT_TIMEOUT)
+            # Reset TTL if get params are passed
+            for key in keys:
+                cache.touch(key, DEFAULT_TIMEOUT)
         return Response(cache.get_many(keys), status=status.HTTP_200_OK)
 
     def post(self, request):
-        post_data = request.data
-        cache.set_many(request.data)
-        keys = [key for key in request.data]
-        return Response(cache.get_many(keys), status=status.HTTP_201_CREATED)
+        # Reset TTL
+        cache.set_many(request.data, DEFAULT_TIMEOUT)
+        return Response(cache.get_many(request.data.keys()), status=status.HTTP_201_CREATED)
 
     def patch(self, request):
-        patch_data = request.data
-        cache.set_many(patch_data)
-        keys = [key for key in request.data]
         # Reset TTL
-        for key in keys:
-            cache.touch(key, DEFAULT_TIMEOUT)
-        return Response(cache.get_many(keys), status=status.HTTP_200_OK)
+        cache.set_many(request.data, DEFAULT_TIMEOUT)
+        return Response(cache.get_many(request.data.keys()), status=status.HTTP_200_OK)
